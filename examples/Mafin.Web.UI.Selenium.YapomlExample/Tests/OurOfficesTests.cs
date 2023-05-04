718e9dc749920eebbd2ac3034ce283d6521f8bc9
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2023-05-04 23:43:23 +0300

    Even more simplified

--- examples/Mafin.Web.UI.Selenium.YapomlExample/Tests/OurOfficesTests.cs
+++ examples/Mafin.Web.UI.Selenium.YapomlExample/Tests/OurOfficesTests.cs
@@ -12,8 +12,8 @@ public class OurOfficesTests : AbstractTest
         Ya.HomePage.ExploreOurClientsWork.Click();
 
         var section = Ya.ClientWorkPage.OurOfficesSection;
-        section.Regions[r => r == "EMEA"].Click();
-        section.Locations[l => l == "BELARUS"].Click();
+        section.Regions["EMEA"].Click();
+        section.Locations["BELARUS"].Click();
         section.DetailsSection.When(it => it.IsDisplayed()).Offices[0].When(it => it.Name.Text.IsNot(string.Empty));
 
         Assert.That(section.DetailsSection.Offices.Select(o => o.Name.Text), Is.EqualTo(expectedOfficeList));
