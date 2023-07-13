3804c78bdd107bab1986743a621faa30b3a275b6
Yauhen Klimiashuk <Yauhen_Klimiashuk@epam.com>
2023-07-13 23:06:52 +0200

    Improve styling on DummyDriver

--- tests/Mafin.Web.UI.Selenium.Tests.Unit/TestDoubles/DummyDriver.cs
+++ tests/Mafin.Web.UI.Selenium.Tests.Unit/TestDoubles/DummyDriver.cs
@@ -2,43 +2,42 @@ using System.Collections.ObjectModel;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
 
-namespace Mafin.Web.UI.Selenium.Tests.Unit.TestDoubles
+namespace Mafin.Web.UI.Selenium.Tests.Unit.TestDoubles;
+
+public sealed class DummyDriver : IWebDriver, IActionExecutor
 {
-    public sealed class DummyDriver : IWebDriver, IActionExecutor
+    public string? Url { get; set; }
+
+    public string? Title { get; }
+
+    public string? PageSource { get; }
+
+    public string? CurrentWindowHandle { get; }
+
+    public ReadOnlyCollection<string>? WindowHandles { get; }
+
+    public bool IsActionExecutor { get; }
+
+    public void Close() => throw new NotImplementedException();
+
+    public IWebElement FindElement(By by) => throw new NotImplementedException();
+
+    public ReadOnlyCollection<IWebElement> FindElements(By by) => throw new NotImplementedException();
+
+    public IOptions Manage() => throw new NotImplementedException();
+
+    public INavigation Navigate() => throw new NotImplementedException();
+
+    public void PerformActions(IList<ActionSequence> actionSequenceList) => throw new NotImplementedException();
+
+    public void Quit() => throw new NotImplementedException();
+
+    public void ResetInputState() => throw new NotImplementedException();
+
+    public ITargetLocator SwitchTo() => throw new NotImplementedException();
+
+    public void Dispose()
     {
-        public string? Url { get; set; }
-
-        public string? Title { get; }
-
-        public string? PageSource { get; }
-
-        public string? CurrentWindowHandle { get; }
-
-        public ReadOnlyCollection<string>? WindowHandles { get; }
-
-        public bool IsActionExecutor { get; }
-
-        public void Close() => throw new NotImplementedException();
-
-        public IWebElement FindElement(By by) => throw new NotImplementedException();
-
-        public ReadOnlyCollection<IWebElement> FindElements(By by) => throw new NotImplementedException();
-
-        public IOptions Manage() => throw new NotImplementedException();
-
-        public INavigation Navigate() => throw new NotImplementedException();
-
-        public void PerformActions(IList<ActionSequence> actionSequenceList) => throw new NotImplementedException();
-
-        public void Quit() => throw new NotImplementedException();
-
-        public void ResetInputState() => throw new NotImplementedException();
-
-        public ITargetLocator SwitchTo() => throw new NotImplementedException();
-
-        public void Dispose()
-        {
-            // Intentionally empty
-        }
+        // Intentionally empty
     }
 }
